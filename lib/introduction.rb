e3e1e223a4b17935e2dbdc02b79ab5180506e382
fortuitous-prototype-dfa5 <lindsay.chen@yale.edu>
2021-05-21 19:40:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@
 def introduction (name, language = Ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction "Lindsay", "Ruby"
+introduction "Lindsay"
 
 
 
